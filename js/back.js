23982cd5e522bd22af218dc4b2fa70c646d73979
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2019-02-22 21:59:11 +0530

    Fix fetch issue, add loading to colleges, add status to registration and disable button

--- js/back.js
+++ js/back.js
@@ -1,28 +1,52 @@
 var baseUrl = 'https://bits-apogee.org/2019/collegeambassador';
+var baseUrl1 = 'https://www.bits-apogee.org/2019/collegeambassador';
 
 var getCollegeUrl = baseUrl + '/college';
-var registerUrl = baseUrl + '/register'
-
-
+var getCollegeUrl1 = baseUrl1 + '/college';
+var registerUrl = baseUrl + '/register';
+var registerUrl1 = baseUrl1 + '/register';
 
 function populateCollegeField() {
     fetch(getCollegeUrl)
-    .then(res => res.json())
-    .then(data => {
-        data = data.data;
-        data.forEach(element => {
-            var name = element.name;
-            var id = element.id;
-            var option = document.createElement('option');
-            option.value = id;
-            option.innerHTML = name;
-            document.getElementById('regform-collegeselect').append(option);
+        .then(res => res.json())
+        .then(data => {
+            data = data.data;
+            data.forEach(element => {
+                var name = element.name;
+                var id = element.id;
+                var option = document.createElement('option');
+                option.value = id;
+                option.innerHTML = name;
+                document.getElementById('regform-collegeselect').append(option);
+            });
+            document.getElementById('loading-colleges').style.display = "none";
+            document.getElementById('regform-collegeselect').disabled = false;
+        })
+        .catch(err => {
+            console.log(err);
+            fetch(getCollegeUrl1)
+                .then(res => res.json())
+                .then(data => {
+                    data = data.data;
+                    data.forEach(element => {
+                        var name = element.name;
+                        var id = element.id;
+                        var option = document.createElement('option');
+                        option.value = id;
+                        option.innerHTML = name;
+                        document.getElementById('regform-collegeselect').append(option);
+                    });
+                    document.getElementById('loading-colleges').style.display = "none";
+                    document.getElementById('regform-collegeselect').disabled = false;
+                })
+                .catch(err => {
+                    console.log(err);
+                    document.getElementById('loading-colleges').innerHTML = "Error occurred while loading colleges!";
+                });
         });
-    })
-    .catch(console.log);
 }
 
-function onSubmitRegister(){
+function onSubmitRegister() {
     var first = document.getElementById('regform-first').value;
     // var last = document.getElementById('regform-last').value;
     var email = document.getElementById('regform-email').value;
@@ -36,6 +60,9 @@ function onSubmitRegister(){
     //var goodCand = document.getElementById('regform-goodcand').value;
     //var pastExp = document.getElementById('regform-pastexp').value;
     //var hasMailingList = document.getElementById('regform-boolmailinglist').value === "Yes" ? true : false;
+
+    document.getElementById('submit').disabled = true;
+    document.getElementById('msg').innerHTML = "Registering. Please wait...";
 
     var requestObject = {
         email: email,
@@ -52,33 +79,59 @@ function onSubmitRegister(){
         city: city,
         college: college,
     };
-    console.log(requestObject);
-    console.log(JSON.stringify(requestObject));
 
     fetch(registerUrl, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        redirect: "follow",
-        body: JSON.stringify(requestObject)
-    }).then(res => res.json())
-    .then(data => {
-        console.log(data);
-        const {message} = data;
-        if(message){
-            document.getElementById('msg').innerHTML = message;
-        }
-        else{
-            document.getElementById('msg').innerHTML = "Unknown error occured, contact administrator";
-        }
-    })
-    .catch(console.log);
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            redirect: "follow",
+            body: JSON.stringify(requestObject)
+        }).then(res => res.json())
+        .then(data => {
+            document.getElementById('submit').disabled = false;
+
+            const {
+                message
+            } = data;
+            if (message) {
+                document.getElementById('msg').innerHTML = message;
+            } else {
+                document.getElementById('msg').innerHTML = "Unknown error occured, contact administrator";
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            fetch(registerUrl1, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    redirect: "follow",
+                    body: JSON.stringify(requestObject)
+                }).then(res => res.json())
+                .then(data => {
+                    document.getElementById('submit').disabled = false;
+                    const {
+                        message
+                    } = data;
+                    if (message) {
+                        document.getElementById('msg').innerHTML = message;
+                    } else {
+                        document.getElementById('msg').innerHTML = "Unknown error occured, contact administrator";
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    document.getElementById('submit').disabled = false;
+                    document.getElementById('msg').innerHTML = "Unknown error occured, contact administrator";
+                });
+        });
 }
 
 populateCollegeField();
 
-document.getElementsByClassName('regform-submit')[0].addEventListener('click', (e) => {
+document.getElementById('submit').addEventListener('click', (e) => {
     e.preventDefault();
     onSubmitRegister();
 });
